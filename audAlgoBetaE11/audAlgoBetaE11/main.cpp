b18c5c52feff0925bf4f835a1f4a40de4ca4b761
Byrdann Fox <mobileupstate@gmail.com>
2014-07-02 04:35:07 -0400

    Updates...

---
+++ audAlgoBetaE11/audAlgoBetaE11/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  audAlgoBetaE11
+//
+//  Created by Byrdann Fox on 7/2/14.
+//  Copyright (c) 2014 ExcepApps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    
+}
+
