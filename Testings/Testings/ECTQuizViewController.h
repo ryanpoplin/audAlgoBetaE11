aa6e8dd02bf4a2e5b83addad010d8702c5a5a721
Byrdann Fox <mobileupstate@gmail.com>
2014-07-04 03:29:55 -0400

    Updates...

---
+++ Testings/Testings/ECTQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECTQuizViewController.h
+//  Testings
+//
+//  Created by Byrdann Fox on 7/4/14.
+//  Copyright (c) 2014 ExcepApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECTQuizViewController : UIViewController
+
+@end
---
+++ Testings/Testings/ECTQuizViewController.m
@@ -0,0 +1,38 @@
+//
+//  ECTQuizViewController.m
+//  Testings
+//
+//  Created by Byrdann Fox on 7/4/14.
+//  Copyright (c) 2014 ExcepApps. All rights reserved.
+//
+
+#import "ECTQuizViewController.h"
+
+@interface ECTQuizViewController ()
+
+@end
+
+@implementation ECTQuizViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
