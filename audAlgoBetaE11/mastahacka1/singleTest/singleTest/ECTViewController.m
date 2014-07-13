65e6fa40102681646521e51a732ad75ffa805d7e
Byrdann Fox <mobileupstate@gmail.com>
2014-07-12 20:01:19 -0400

    Updates...

--- audAlgoBetaE11/mastahacka1/singleTest/singleTest/ECTViewController.m
+++ audAlgoBetaE11/mastahacka1/singleTest/singleTest/ECTViewController.m
@@ -18,6 +18,19 @@
 
 - (void)viewDidLoad
 {
+    
+    NSString *testString = @"Objective-C is cool, but Swift is the future...";
+    
+    NSLog(@"%@\n", testString);
+    
+    CGFloat testFloat = M_PI;
+    
+    NSLog(@"%f\n", testFloat);
+    
+    CGFloat *pointerTestFloat = &testFloat;
+    
+    NSLog(@"%p\n", pointerTestFloat);
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
